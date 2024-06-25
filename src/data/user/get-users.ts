8c5567ac1de44a228ab6687089417d5ec909ac05
constROD <rialcantara21@gmail.com>
2024-06-25 22:17:52 +0800

    chore: add sample query but commented just to have an example to get a user specific data

--- src/data/user/get-users.ts
+++ src/data/user/get-users.ts
@@ -9,6 +9,40 @@ export type GetUsersDataArgs = {
   orderBy?: 'asc' | 'desc';
 };
 
+/*
+  * For query that is based on user_id field. This is useful when you want to get records that are specific to a user.
+
+  export async function getBills({
+    dbClient,
+    limit = 25,
+    page = 1,
+    sortBy = 'created_at',
+    orderBy = 'desc',
+    userId,
+  }: GetBillsDataArgs) {
+    let query = dbClient
+      .selectFrom('bills')
+      .selectAll()
+      .limit(limit)
+      .offset((page - 1) * limit)
+      .orderBy(sortBy, orderBy);
+
+    let allRecordsQuery = dbClient
+      .selectFrom('bills')
+      .select(dbClient.fn.count('id').as('total_records'));
+
+    if (userId) {
+      query = query.where('user_id', '=', userId);
+      allRecordsQuery = allRecordsQuery.where('user_id', '=', userId);
+    }
+
+    const records = await query.execute();
+    const allRecords = await allRecordsQuery.executeTakeFirst();
+
+    return { records, totalRecords: Number(allRecords?.total_records) ?? 0 };
+  }
+*/
+
 export async function getUsersData({
   dbClient,
   limit = 25,
