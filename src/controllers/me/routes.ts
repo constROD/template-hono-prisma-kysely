3239fa8607fbc5d029d25f119495518bfbe560ac
constROD <rialcantara21@gmail.com>
2025-01-12 18:15:57 +0800

    chore: add comment for type workaround in routes

--- src/controllers/me/routes.ts
+++ src/controllers/me/routes.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
+// @ts-nocheck NOTE: Remove this when you are changing something in this file and put it back once you are done
 import { type HonoEnv } from '@/types/hono';
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { getMyProfileRoute, getMyProfileRouteHandler } from './get-my-profile';
--- src/controllers/server/routes.ts
+++ src/controllers/server/routes.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
+// @ts-nocheck NOTE: Remove this when you are changing something in this file and put it back once you are done
 import { type HonoEnv } from '@/types/hono';
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { getServerDateTimeRoute, getServerDateTimeRouteHandler } from './get-server-date-time';
