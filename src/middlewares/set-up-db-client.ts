b65bd6b57609a7ad56e001d25646bb94cecd8598
constROD <rialcantara21@gmail.com>
2025-04-14 22:29:12 +0800

    fix: error logging

--- src/middlewares/set-up-db-client.ts
+++ src/middlewares/set-up-db-client.ts
@@ -1,9 +1,10 @@
 import { createDbClient } from '@/db/create-db-client';
+import { type HonoEnv } from '@/types/hono';
 import { type Context, type Next } from 'hono';
 
 const dbClient = createDbClient();
 
-export async function setUpDbClientMiddleware(c: Context, next: Next) {
+export async function setUpDbClientMiddleware(c: Context<HonoEnv>, next: Next) {
   c.set('dbClient', dbClient);
   await next();
 }
