14bc8827335a13564241faba00a255a037231093
constROD <rialcantara21@gmail.com>
2025-04-16 00:17:04 +0800

    fix: error logging

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -5,12 +5,7 @@ import { type Context } from 'hono';
 export async function errorHandlerMiddleware(err: Error, c: Context<HonoEnv>) {
   const { error, statusCode } = makeError(err);
   // const errorContextData = {
-  //   request: {
-  //     params: JSON.stringify(c.req.param(), null, 2),
-  //     query: JSON.stringify(c.req.query(), null, 2),
-  //     headers: JSON.stringify(c.req.header(), null, 2),
-  //     body: JSON.stringify(await c.req.json(), null, 2),
-  //   },
+  //   context: JSON.stringify(c, null, 2),
   //   statusCode: statusCode,
   //   error: error,
   // };
