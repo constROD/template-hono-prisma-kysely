021fb4ce5449a9b095fe5b6048fe0f48aa290990
constROD <rialcantara21@gmail.com>
2025-07-03 18:41:39 +0800

    fix: stages

--- src/env.ts
+++ src/env.ts
@@ -10,7 +10,7 @@ export function isTest() {
 
 const envSchema = z.object({
   APP_PORT: z.coerce.number().default(3000),
-  STAGE: z.nativeEnum(STAGES).default(STAGES.Dev),
+  STAGE: z.nativeEnum(STAGES).default(STAGES.Local),
   DB_URL: z.string(),
   TEST_DB_URL: z.string(),
   DB_MIGRATION_URL: z.string(),
