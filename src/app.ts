4ecfa47981bd96ccdaef304489deb24817abc47d
constROD <rialcantara21@gmail.com>
2025-07-03 15:01:39 +0800

    chore: update rules and readme

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,47 @@
-import { serve } from '@hono/node-server';
 import { swaggerUI } from '@hono/swagger-ui';
 import { OpenAPIHono } from '@hono/zod-openapi';
 import { apiReference } from '@scalar/hono-api-reference';
 import { logger } from 'hono/logger';
 import { version } from '../package.json';
+import { STAGES } from './constants/env';
 import { routes } from './controllers/routes';
 import { schemas } from './data/schema';
 import { envConfig } from './env';
 import { errorHandlerMiddleware } from './middlewares/error-handler';
 import { setUpDbClientMiddleware } from './middlewares/set-up-db-client';
 import type { HonoEnv } from './types/hono';
-import { pinoLogger } from './utils/logger';
 
 const app = new OpenAPIHono<HonoEnv>();
 
-/* API Docs */
-app.doc('/openapi.json', {
-  openapi: '3.0.0',
-  info: {
-    version,
-    title: `${envConfig.STAGE.toUpperCase()} API`,
-    description: 'API Documentation',
-  },
-  externalDocs: {
-    description: 'API Reference',
-    url: '/reference',
-  },
-});
-app.openAPIRegistry.registerComponent('securitySchemes', 'bearerAuth', {
-  type: 'http',
-  scheme: 'bearer',
-  bearerFormat: 'JWT',
-});
+if (envConfig.STAGE !== STAGES.Prod) {
+  /* API Docs */
+  app.doc('/openapi.json', {
+    openapi: '3.0.0',
+    info: {
+      version,
+      title: `${envConfig.STAGE.toUpperCase()} API`,
+      description: 'API Documentation',
+    },
+    externalDocs: {
+      description: 'API Reference',
+      url: '/reference',
+    },
+  });
+  app.openAPIRegistry.registerComponent('securitySchemes', 'bearerAuth', {
+    type: 'http',
+    scheme: 'bearer',
+    bearerFormat: 'JWT',
+  });
 
-/* Register Schemas */
-Object.entries(schemas).forEach(([key, value]) => {
-  app.openAPIRegistry.register(key, value);
-});
+  /* Register Schemas */
+  Object.entries(schemas).forEach(([key, value]) => {
+    app.openAPIRegistry.register(key, value);
+  });
 
-/* API Docs */
-app.get('/swagger', swaggerUI({ url: '/openapi.json' }));
-app.get('/reference', apiReference({ spec: { url: '/openapi.json' } }));
+  /* API Docs */
+  app.get('/swagger', swaggerUI({ url: '/openapi.json' }));
+  app.get('/reference', apiReference({ spec: { url: '/openapi.json' } }));
+}
 
 /* Global Middlewares */
 app.onError(errorHandlerMiddleware);
@@ -50,11 +51,6 @@ app.use(setUpDbClientMiddleware);
 /* Routes */
 routes.forEach(route => {
   app.route('/', route);
-});
-
-/* Server */
-serve({ fetch: app.fetch, port: envConfig.APP_PORT }, info => {
-  pinoLogger.info('Listening on port', info.port);
 });
 
 export default app;
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/app.ts'],
+  entry: ['src/server.ts'],
   format: 'esm',
   dts: true,
   splitting: false,
