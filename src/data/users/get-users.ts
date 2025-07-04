cf888edf186359c811c6e774c8de7d4bebd8e9ea
constROD <rialcantara21@gmail.com>
2025-07-04 21:18:50 +0800

    chore: update test

--- src/data/users/get-users.ts
+++ src/data/users/get-users.ts
@@ -1,6 +1,6 @@
 import type { DbClient } from '@/db/create-db-client';
 import type { User } from '@/db/schema';
-import { makeDefaultDataListReturn } from '../make-default-list-return';
+import { transformToPaginatedResponse } from '../transform-to-paginated-response';
 
 export type GetUsersDataArgs = {
   dbClient: DbClient;
@@ -36,7 +36,7 @@ export async function getUsersData({
     .select(eb => eb.fn.count('id').as('total_records'))
     .executeTakeFirst();
 
-  return makeDefaultDataListReturn({
+  return transformToPaginatedResponse({
     records,
     totalRecords: Number(allRecords?.total_records) ?? 0,
     limit,
