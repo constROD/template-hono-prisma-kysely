70114c83d98c8fafb30f143d194c5145998c4325
constrod <rialcantara21@gmail.com>
2025-08-26 04:22:16 +0800

    chore: add logger

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -1,11 +1,11 @@
 import type { HonoEnv } from '@/types/hono';
 import { makeError } from '@/utils/errors';
-import { pinoLogger } from '@/utils/logger';
+import { logger } from '@/utils/logger';
 import type { Context } from 'hono';
 
 export async function errorHandlerMiddleware(err: Error, c: Context<HonoEnv>) {
   const { error, statusCode } = makeError(err);
-  pinoLogger.error(error.message, error);
+  logger.error(error.message, error);
   // const errorContextData = {
   //   context: JSON.stringify(c, null, 2),
   //   statusCode: statusCode,
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,28 +1,26 @@
-import pino from 'pino';
-
-const pinoConfig = pino({
-  timestamp: pino.stdTimeFunctions.isoTime,
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: false,
-    },
-  },
-});
+/* eslint-disable no-console */
 
 function formatLogMessage<TLogDataOrError>(message: string, data?: TLogDataOrError) {
+  const timestamp = new Date().toISOString();
   const logMessage = data ? `${message} ${JSON.stringify(data, null, 2)}` : message;
-  return logMessage;
+  return { timestamp, message: logMessage };
 }
 
-export const pinoLogger = {
-  info: <TLogDataOrError>(message: string, data?: TLogDataOrError) =>
-    pinoConfig.info(formatLogMessage(message, data)),
-  error: <TLogDataOrError>(message: string, data?: TLogDataOrError) =>
-    pinoConfig.error(formatLogMessage(message, data)),
-  debug: <TLogDataOrError>(message: string, data?: TLogDataOrError) =>
-    pinoConfig.debug(formatLogMessage(message, data)),
-  warn: <TLogDataOrError>(message: string, data?: TLogDataOrError) =>
-    pinoConfig.warn(formatLogMessage(message, data)),
+export const logger = {
+  info: <TLogDataOrError>(message: string, data?: TLogDataOrError) => {
+    const { timestamp, message: logMessage } = formatLogMessage(message, data);
+    console.log(`[${timestamp}] INFO:`, logMessage);
+  },
+  error: <TLogDataOrError>(message: string, data?: TLogDataOrError) => {
+    const { timestamp, message: logMessage } = formatLogMessage(message, data);
+    console.error(`[${timestamp}] ERROR:`, logMessage);
+  },
+  debug: <TLogDataOrError>(message: string, data?: TLogDataOrError) => {
+    const { timestamp, message: logMessage } = formatLogMessage(message, data);
+    console.debug(`[${timestamp}] DEBUG:`, logMessage);
+  },
+  warn: <TLogDataOrError>(message: string, data?: TLogDataOrError) => {
+    const { timestamp, message: logMessage } = formatLogMessage(message, data);
+    console.warn(`[${timestamp}] WARN:`, logMessage);
+  },
 };
