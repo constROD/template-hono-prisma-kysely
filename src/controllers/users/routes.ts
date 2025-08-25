fde785209f6d5e409b564e1385fa1f3023a38fc3
constrod <rialcantara21@gmail.com>
2025-08-26 04:23:24 +0800

    chore: add logger

--- src/controllers/users/routes.ts
+++ src/controllers/users/routes.ts
@@ -4,14 +4,12 @@ import { archiveUserRoute, archiveUserRouteHandler } from './archive-user';
 import { createUserRoute, createUserRouteHandler } from './create-user';
 import { deleteUserRoute, deleteUserRouteHandler } from './delete-user';
 import { getUserRoute, getUserRouteHandler } from './get-user';
-import { getUsersRoute, getUsersRouteHandler } from './get-users';
 import { searchUsersRoute, searchUsersRouteHandler } from './search-users';
 import { unarchiveUserRoute, unarchiveUserRouteHandler } from './unarchive-user';
 import { updateUserRoute, updateUserRouteHandler } from './update-user';
 
 const usersRoutes = new OpenAPIHono<HonoEnv>()
   .openapi(searchUsersRoute, searchUsersRouteHandler)
-  .openapi(getUsersRoute, getUsersRouteHandler)
   .openapi(createUserRoute, createUserRouteHandler)
   .openapi(getUserRoute, getUserRouteHandler)
   .openapi(updateUserRoute, updateUserRouteHandler)
