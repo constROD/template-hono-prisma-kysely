2f0ac141b10324d516784159c169b7ee54ade93c
constrod <rialcantara21@gmail.com>
2025-08-26 04:20:57 +0800

    chore: add logger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import { serve } from '@hono/node-server';
 import app from './app';
 import { getEnvConfig } from './env';
-import { pinoLogger } from './utils/logger';
+import { logger } from './utils/logger';
 
 const env = getEnvConfig();
 const mainAppEntry = app;
 
 serve({ fetch: mainAppEntry.fetch, port: env.APP_PORT }, info => {
-  pinoLogger.info('Listening on port', info.port);
+  logger.info('Listening on port', info.port);
 });
